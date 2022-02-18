f66bac19850b1f043748b9089dd88f8d309884cc
JohanLx <launchx02750@innovaccion.mx>
2022-02-17 19:11:56 -0600

    primera entrega

---
+++ Modulo2Katas.py
@@ -0,0 +1,11 @@
+from datetime import *
+from dateutil.relativedelta import *
+
+now = datetime.now()
+print(now)
+
+parsec = 11
+lightyears = 3.26156 * parsec
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
+
